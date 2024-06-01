4f244d2d62a586fe3b8dc5525ea74b5c545a765c
Helio <helio609.dev@outlook.com>
2024-06-01 19:27:56 +0800

    add google ads

--- app/(web)/layout.tsx
+++ app/(web)/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from '@/components/footer'
 import Header from '@/components/header'
+import Script from 'next/script'
 import { Toaster } from 'sonner'
 
 export default function WebRootLayout({
@@ -8,11 +9,18 @@ export default function WebRootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-1">{children}</main>
-      <Toaster />
-      <Footer />
-    </div>
+    <>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5329411986081309"
+        crossOrigin="anonymous"
+      />
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-1">{children}</main>
+        <Toaster />
+        <Footer />
+      </div>
+    </>
   )
 }
