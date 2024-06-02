624f83690698dc70b22ed283cee17b0c6271c4b6
Helio <helio609.dev@outlook.com>
2024-06-02 16:55:53 +0800

    move google ads to root layout and enable rich colors in toast

--- app/(web)/layout.tsx
+++ app/(web)/layout.tsx
@@ -1,6 +1,5 @@
 import Footer from '@/components/footer'
 import Header from '@/components/header'
-import Script from 'next/script'
 import { Toaster } from 'sonner'
 
 export default function WebRootLayout({
@@ -9,18 +8,11 @@ export default function WebRootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <>
-      <Script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5329411986081309"
-        crossOrigin="anonymous"
-      />
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-1">{children}</main>
-        <Toaster />
-        <Footer />
-      </div>
-    </>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Toaster richColors />
+      <Footer />
+    </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import type { Metadata } from 'next'
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Notion Statistics',
@@ -15,6 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5329411986081309"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body>
         <ThemeProvider
           attribute="class"
